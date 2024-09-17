76235cf1fddc408b0f3aeffabb3febe573498c0b
Guillaume Bedard <bgui34@gmail.com>
2024-09-16 22:54:44 -0400

    feat: Added navigation header

---
+++ components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script lang="ts"></script>
+
+<template>
+  <nav class="bg-transparent absolute top-4 left-0 right-0 z-10">
+    <div class="container mx-auto flex justify-between items-center">
+      <h1 class="text-2xl font-bold text-primary mb-4">HypnoBot</h1>
+      <a class="text-2xl mb-8 text-secondary" href="/login">Login</a>
+    </div>
+  </nav>
+</template>
