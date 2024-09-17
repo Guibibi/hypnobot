a512cd24f6d8a49fb48cc5e2b7a35776942a31c1
Guillaume Bedard <bgui34@gmail.com>
2024-09-16 22:09:44 -0400

    feat: Add Base Button component

---
+++ components/BaseButton.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+interface Props {
+  text: string;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: "button",
+  disabled: false,
+});
+
+const emit = defineEmits<{
+  (e: "click", event: MouseEvent): void;
+}>();
+
+const handleClick = (event: MouseEvent) => {
+  emit("click", event);
+};
+</script>
+
+<template>
+  <button
+    :type="type"
+    :disabled="disabled"
+    class="capitalize py-2 px-4 bg-accent text-white rounded-xl font-bold text-lg"
+    @click="handleClick"
+  >
+    {{ text }}
+  </button>
+</template>
