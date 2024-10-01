91b7035cb3a59439d9fda2778613d66a4919788d
Guillaume Bedard <g.bedard@appcom.ca>
2024-10-01 13:03:20 -0400

    Add register page for email / Add login in with email.

--- pages/register.vue
+++ pages/register.vue
@@ -2,24 +2,18 @@
 const supabase = useSupabaseClient();
 const email = ref("");
 const password = ref("");
+const confirmPassword = ref("");
 const errorMessage = ref("");
 
 const isFormValid = computed(
-  () => email.value.trim() !== "" && password.value.trim() !== "",
+  () =>
+    email.value.trim() !== "" &&
+    password.value.trim() !== "" &&
+    password.value.trim() === confirmPassword.value.trim(),
 );
 
-const signInWithOtp = async () => {
-  const { error } = await supabase.auth.signInWithOtp({
-    email: email.value,
-    options: {
-      emailRedirectTo: "http://localhost:3000/confirm",
-    },
-  });
-  if (error) console.log(error);
-};
-
-const signInWithPassword = async () => {
-  const { error } = await supabase.auth.signInWithPassword({
+const signUpWithPassword = async () => {
+  const { error } = await supabase.auth.signUp({
     email: email.value,
     password: password.value,
   });
@@ -39,64 +33,55 @@ const signInWithPassword = async () => {
         </h2>
         <p class="mt-2 text-sm text-gray-600">Enter your email to login</p>
       </div>
-      <form class="mt-8 space-y-6" @submit.prevent="signInWithOtp">
+      <form class="mt-8 space-y-6" @submit.prevent="signUpWithPassword">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
-            <label for="email-phone" class="sr-only"
-              >Email or Phone Number</label
-            >
+            <label for="email" class="sr-only">Email or Phone Number</label>
             <input
-              id="email-phone"
+              id="email"
               v-model="email"
-              name="email-phone"
+              name="email"
               type="text"
               required
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email or Phone Number"
+              placeholder="Email"
             />
 
             <div class="mt-4">
-              <label for="email-phone" class="sr-only">Password</label>
+              <label for="password" class="sr-only">Password</label>
               <input
-                id="email-phone"
+                id="password"
                 v-model="password"
-                name="passwprd"
+                name="password"
                 type="password"
                 required
                 class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
             </div>
+            <div class="mt-4">
+              <label for="confirm-password" class="sr-only"
+                >Confirm Password</label
+              >
+              <input
+                id="confirm-password"
+                v-model="confirmPassword"
+                name="confirm-password"
+                type="password"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Confirm Password"
+              />
+            </div>
           </div>
         </div>
 
         <div>
-          <button
-            type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <svg
-                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
-            Send Login Code
-          </button>
           <BaseButton
             :disabled="!isFormValid"
             class="mt-4"
             text="Login"
-            @click="signInWithPassword"
+            @click="signUpWithPassword"
             >Login</BaseButton
           >
           {{ errorMessage }}
