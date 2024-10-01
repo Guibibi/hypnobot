b2bb2f687286c25e0315d4b01a758255d4efcd79
Guillaume Bedard <g.bedard@appcom.ca>
2024-10-01 13:02:29 -0400

    Add page exclusion for non-logged in user

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: {
     enabled: true,
 
     timeline: {
-      enabled: true
-    }
+      enabled: true,
+    },
   },
   modules: [
-    '@nuxtjs/supabase',
-    '@nuxtjs/i18n',
-    'radix-vue',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/eslint'
-  ]
-})
+    "@nuxtjs/supabase",
+    "@nuxtjs/i18n",
+    "radix-vue",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/eslint",
+  ],
+  supabase: {
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      exclude: ["/", "/register", "/login"],
+    },
+  },
+});
+
